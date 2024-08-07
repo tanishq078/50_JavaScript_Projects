0243bb41f1004794f633c0437d8ba466bd5d2673
tanishq078 <stanishq69@gmail.com>
2024-08-07 22:49:10 +0530

    Created a QR Code Generator

---
+++ QR Code Generator/qrcode.js
@@ -0,0 +1,17 @@
+const qrinput = document.getElementById('qr-input');
+
+const qrimg = document.getElementById('qr-img');
+
+const qrbutton = document.getElementById('qr-button');
+
+qrbutton.addEventListener('click', ()=>{
+    const inputValue = qrinput.value;
+    if(!inputValue){
+        alert('Please enter a valid input!');
+        return;
+    }
+    else{
+        qrimg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+        qrimg.alt = `QR Code for ${inputValue}`;
+    }
+})
